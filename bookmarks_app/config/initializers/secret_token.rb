af82887730c3ba5858bd759dded069a6d2b388da
Kai Bruesch <bbruesch@mail.csuchico.edu>
2012-10-16 06:24:10 -0700

    Revert "Broken Server"
    This reverts commit fd5f05d42b3c6a0ab2ca50058679efea3c5ddc69.

--- bookmarks_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BookmarksApp::Application.config.secret_token = '3dfdf716231e3feaafaa6d61d2d1b185f140af63e4d1d343eba2cde30006a3f3745db3fd0adc8de6d614a9279513c038f8bf34c5e402f6763d091d8fc1306878'
